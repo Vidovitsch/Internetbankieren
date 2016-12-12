dbbcfe387197a937b8895f6402106446dd2e8b07
Bekker,David D.S.H. de <david.debekker@student.fontys.nl>
2016-12-13 00:23:44 +0100

    Client Shared_Centrale added because of exception

--- IB_Client/src/ib_client/GUIController.java
+++ IB_Client/src/ib_client/GUIController.java
@@ -1,11 +1,14 @@
 package ib_client;
 
 import Shared_Client.IAdmin;
-import java.rmi.AccessException;
+import fontyspublisher.IRemotePropertyListener;
+import fontyspublisher.IRemotePublisherForListener;
+import java.beans.PropertyChangeEvent;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,21 +17,30 @@ import java.util.logging.Logger;
  *
  * @author David
  */
-public class GUIController {
+public class GUIController extends UnicastRemoteObject implements IRemotePropertyListener {
 
     private GUI gui;
     private IAdmin admin;
+    private IRemotePublisherForListener publisher;
     
     /**
      * Handles all RMI-based processes. Updates the GUI
      * @param gui
+     * @throws java.rmi.RemoteException
      */
-    public GUIController(GUI gui) {
+    public GUIController(GUI gui) throws RemoteException {
         try {
             Registry serverRegistry = LocateRegistry.getRegistry("localhost", 1099);
             admin = (IAdmin) serverRegistry.lookup("admin");
+            publisher = (IRemotePublisherForListener) serverRegistry.lookup("serverPublisher");
+            System.out.println("Lookup succesful");
         } catch (RemoteException | NotBoundException ex) {
             Logger.getLogger(GUIController.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent pce) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
