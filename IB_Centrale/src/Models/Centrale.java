877654dae89654082145d707658b822e377efb88
Bekker,David D.S.H. de <david.debekker@student.fontys.nl>
2016-12-19 14:03:48 +0100

    Centrale getTransactions update (merge solved)

--- IB_Centrale/src/Models/Centrale.java
+++ IB_Centrale/src/Models/Centrale.java
@@ -5,6 +5,7 @@ import Shared_Centrale.ITransactie;
 import Shared_Centrale.IBankTrans;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 /**
@@ -22,12 +23,27 @@ public class Centrale extends UnicastRemoteObject implements ICentrale {
     public void startTransaction(String IBAN1, String IBAN2, IBankTrans bank, double value, String description) throws RemoteException {
         bank.removeSaldo(IBAN1, value);
         bank.addSaldo(IBAN2, value);
-        transactions.add(null);
+        
+        //Add a new transaction to the centrale
+        Transactie transactie = new Transactie(IBAN1, IBAN2, getCurrentDateTime(), value);
+        if (!description.isEmpty()) transactie.setDescription(description);
+        transactions.add(transactie);
     }
 
     @Override
     public ArrayList<ITransactie> getTransactions(String IBAN) throws RemoteException {
-        return null;
+        ArrayList<ITransactie> transList = new ArrayList();
+        for (ITransactie trans : transactions) {
+            if (trans.getIBANTo().equals(IBAN) || trans.getIBANFrom().equals(IBAN)) {
+                transList.add(trans);
+            }
+        }
+        return transList;
     }
 
+    private String getCurrentDateTime() {
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime.toString());
+        return dateTime.toString();
+    }
 }
--- IB_Centrale/src/Models/Transactie.java
+++ IB_Centrale/src/Models/Transactie.java
@@ -15,7 +15,6 @@ public class Transactie extends UnicastRemoteObject implements ITransactie {
     private String date;
     private double amount;
     private String description;
-    private IBankTrans bank;
     private String IBAN1;
     private String IBAN2;
             
@@ -26,13 +25,11 @@ public class Transactie extends UnicastRemoteObject implements ITransactie {
      * @param IBAN2
      * @param date
      * @param amount
-     * @param bank
      * @throws RemoteException 
      */
-    public Transactie(String IBAN1, String IBAN2, String date, double amount, IBankTrans bank) throws RemoteException {
+    public Transactie(String IBAN1, String IBAN2, String date, double amount) throws RemoteException {
         this.date = date;
         this.amount = amount;
-        this.bank = bank;
         this.IBAN1 = IBAN1;
         this.IBAN2 = IBAN2;
     }
@@ -43,26 +40,26 @@ public class Transactie extends UnicastRemoteObject implements ITransactie {
 
     @Override
     public String getDate() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return date;
     }
 
     @Override
     public double getAmount() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return amount;
     }
 
     @Override
     public String getDescription() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return description;
     }
 
     @Override
     public String getIBANFrom() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return IBAN1;
     }
 
     @Override
     public String getIBANTo() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return IBAN2;
     }
 }
