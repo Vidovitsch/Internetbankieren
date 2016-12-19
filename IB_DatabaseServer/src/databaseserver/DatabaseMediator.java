ec1771dc4dea7feb96fead929faf5be5763323c4
Eijkeren,Michiel M.G.B. van <m.vaneijkeren@student.fontys.nl>
2016-12-19 17:34:51 +0100

    niet veel

--- IB_DatabaseServer/src/databaseserver/DatabaseMediator.java
+++ IB_DatabaseServer/src/databaseserver/DatabaseMediator.java
@@ -5,7 +5,7 @@
  */
 package databaseserver;
 
-import Shared.Interfaces.IPersistencyMediator;
+import Shared_Data.IPersistencyMediator;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.Connection;
