4467acb1f26b76eca56774475c658ac4ab44b7fe
Bekker,David D.S.H. de <david.debekker@student.fontys.nl>
2016-12-20 22:38:38 +0100

    Server AdminTest added (+ login tests finished)

---
+++ IB_Server/test/AdminTest.java
@@ -0,0 +1,179 @@
+import Exceptions.LoginException;
+import Models.Administratie;
+import Shared_Centrale.IBankTrans;
+import Shared_Centrale.ICentrale;
+import Shared_Client.Klant;
+import Shared_Data.IPersistencyMediator;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author David
+ */
+
+//Zet de centrale en de database aan voor deze tests
+public class AdminTest {
+    
+    private final String ipAddressDB = "localhost";
+    private final String bindingName = "Database";
+    private final int portNumber = 1088;
+    private Administratie admin;
+    
+    public AdminTest() {
+        try {
+            Registry dataBaseRegistry = LocateRegistry.getRegistry(ipAddressDB, portNumber);
+            IPersistencyMediator database = (IPersistencyMediator) dataBaseRegistry.lookup(bindingName);
+            admin = new Administratie(new ICentrale() {
+                @Override
+                public void startTransaction(String IBAN1, String IBAN2, IBankTrans bank, double value, String description) throws RemoteException {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+                
+                @Override
+                public ArrayList<String> getTransactions(String IBAN) throws RemoteException {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+            });
+            admin.setPersistencyMediator(database);
+        } catch (RemoteException | NotBoundException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    //Tests the exception thrown when trying to login with an empty name
+    @Test
+    public void emptyNameLoginTest() {
+            /**
+             * Logs in a user and creating a session if succesful.
+             * @param naam
+             * @param woonplaats
+             * @param password not null, else IllegalArgumentException.
+             * @return Klant object if succesfull, else null.
+             * @throws Exceptions.LoginException
+             * @throws IllegalArgumentException
+             * @throws RemoteException
+             */
+        try {
+            admin.login("", "woonplaats", "wachtwoord");
+            fail();
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            assertTrue(true);
+        } catch (LoginException | RemoteException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+    
+    //Tests the exception thrown when trying to login with an empty residence
+    @Test
+    public void emptyResidenceLoginTest() {
+        try {
+            admin.login("naam", "", "wachtwoord");
+            fail();
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            assertTrue(true);
+        } catch (LoginException | RemoteException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+    
+    //Tests the exception thrown when trying to login with an empty password
+    @Test
+    public void emptyPasswordLoginTest() {
+        try {
+            admin.login("naam", "woonplaats", "");
+            fail();
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            assertTrue(true);
+        } catch (LoginException | RemoteException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+    
+    //Tests the exception thrown when trying to login with an invalid username
+    @Test
+    public void invalidUsernameLoginTest() {
+        try {
+            admin.login("DummyUser", "InvalidUser", "123456789");
+            fail();
+        } catch (LoginException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            assertTrue(true);
+        } catch (IllegalArgumentException | RemoteException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+    
+    //Tests the exception thrown when trying to login with an invalid username
+    @Test
+    public void invalidPasswordLoginTest() {
+        try {
+            admin.login("DummyUser", "DummyUser", "InvalidPassword");
+            fail();
+        } catch (LoginException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            assertTrue(true);
+        } catch (IllegalArgumentException | RemoteException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+    
+    //Tests if the client gets a valid Klant object after login
+    @Test
+    public void validKlantOnLogin() {
+        try {
+            Klant klant = admin.login("DummyUser", "DummyUser", "123456789");
+            assertEquals("Username is DummyUserDummyUser", "DummyUserDummyUser", klant.getUsername());
+        } catch (LoginException | IllegalArgumentException | RemoteException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+    
+    //Tests if the client gets a valid session after login
+    @Test
+    public void validSessionOnLogin() {
+        try {
+            Klant klant = admin.login("DummyUser", "DummyUser", "123456789");
+            assertTrue("This client has a valid session", admin.checkSession(klant));
+        } catch (LoginException | IllegalArgumentException | RemoteException ex) {
+            Logger.getLogger(AdminTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
